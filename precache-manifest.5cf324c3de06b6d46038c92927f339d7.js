38b65fa66ab73de3aad8869f94daa30f02be822c
morozig <morozig@gmail.com>
2019-11-05 14:22:18 +0300

    Updates

---
+++ precache-manifest.5cf324c3de06b6d46038c92927f339d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ce6ae97719779aa6bb62fd4bc8904a",
+    "url": "/abstract-strategy-games/index.html"
+  },
+  {
+    "revision": "af915cb39a277c7511d8",
+    "url": "/abstract-strategy-games/static/css/main.64537864.chunk.css"
+  },
+  {
+    "revision": "bab9d66aa3b5d29df694",
+    "url": "/abstract-strategy-games/static/js/2.d5a299f0.chunk.js"
+  },
+  {
+    "revision": "af915cb39a277c7511d8",
+    "url": "/abstract-strategy-games/static/js/main.538c0e72.chunk.js"
+  },
+  {
+    "revision": "55aae3b1fd4315a5ef1b",
+    "url": "/abstract-strategy-games/static/js/runtime~main.59627a93.js"
+  }
+]);
