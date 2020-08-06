a9b7b8ef6fae254d60138fa3c9f3e77a2ab13530
morozig <morozig@gmail.com>
2020-08-06 20:45:30 +0300

    node js api

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,14 @@
 const prod = process.env.NODE_ENV === 'production';
+const isNode = typeof window === 'undefined';
+const rootLocation = isNode ?
+    `file://${process.cwd()}` :
+    `${window.location}`;
 
 const config = {
     dataUrl: prod ?
         'https://storage.googleapis.com/abstract-strategy-games-data' :
-        `${window.location}data`
+        `${rootLocation}data`,
+    
 };
 
 export default config;
