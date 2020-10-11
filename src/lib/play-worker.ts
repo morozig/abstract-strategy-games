764b4ef274fd99103b137199e4d812bcc681e827
morozig@gmail.com <morozig@gmail.com>
2020-10-11 15:06:24 +0300

    added threads

---
+++ src/lib/play-worker.ts
@@ -0,0 +1,15 @@
+import {
+  Observable,
+  Subject
+} from 'threads/observable';
+import GameRules from '../interfaces/game-rules';
+import GameHistory from '../interfaces/game-history';
+import PolicyAgent from '../interfaces/policy-agent';
+import PolicyAction from '../interfaces/policy-action';
+import GameModel from '../interfaces/game-model';
+import Alpha from '../agents/alpha';
+import { durationHR } from './helpers';
+
+export default class PlayWorker {
+
+};
