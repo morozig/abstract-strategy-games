19a540bad5303ef6fcba7c92388b77e914486c55
morozig <morozig@gmail.com>
2020-12-05 22:06:43 +0300

    simple batcher

--- src/lib/batcher.ts
+++ src/lib/batcher.ts
@@ -2,59 +2,56 @@ import { sleep } from "./helpers";
 
 type BatchFunc<I,O> = (inputs: I[]) => Promise<O[]>;
 interface Item<I,O> {
-    input: I;
-    resolve: (output: O) => void;
+  input: I;
+  resolve: (output: O) => void;
 }
 
 export default class Batcher<I,O> {
-    private func: BatchFunc<I,O>;
-    private size: number;
-    private wait: number;
-    private onFull: (() => void) | null = null;
-    private queue: Item<I,O>[] = [];
-    constructor(func: BatchFunc<I,O>, size: number, wait: number) {
-        this.func = func;
-        this.size = size;
-        this.wait = wait;
+  private func: BatchFunc<I,O>;
+  private size: number;
+  private wait: number;
+  private timeout: NodeJS.Timeout | null = null;
+  private queue: Item<I,O>[] = [];
+  constructor(func: BatchFunc<I,O>, size: number, wait: number) {
+    this.func = func;
+    this.size = size;
+    this.wait = wait;
+  }
+  private async callBatch() {
+    const items = this.queue.slice(0, this.size);
+    this.queue.splice(0, this.size);
+    const inputs = items.map(item => item.input);
+    const outputs = await this.func(inputs);
+    items.forEach((item, i) => {
+      item.resolve(outputs[i]);
+    });
+  }
+  private add(item: Item<I,O>) {
+    this.queue.push(item);
+    if (this.queue.length === 1) {
+      this.timeout = setTimeout(
+        () => {
+          this.timeout = null;
+          this.callBatch();
+        },
+        this.wait
+      );
     }
-    private batchFull() {
-        return new Promise((resolve: () => void) => {
-            this.onFull = resolve;
-        });
+    if (this.queue.length >= this.size) {
+      if (this.timeout) {
+        clearTimeout(this.timeout);
+        this.timeout = null;
+      }
+      this.callBatch();
     }
-    private async callBatch() {
-        await Promise.race([
-            sleep(this.wait),
-            this.batchFull()
-        ]);
-        // console.log('batch:',this.queue.length);
-        const items = this.queue.slice();
-        this.queue = [];
-        this.onFull = null;
-        const inputs = items.map(item => item.input);
-        const outputs = await this.func(inputs);
-        items.forEach((item, i) => {
-            item.resolve(outputs[i]);
-        });
-    }
-    private add(item: Item<I,O>) {
-        this.queue.push(item);
-        if (this.queue.length === 1) {
-            this.callBatch();
-        }
-        if (this.queue.length === this.size) {
-            if (this.onFull) {
-                this.onFull();
-            }
-        }
-    }
-    call(input: I) {
-        return new Promise((resolve: (output: O) => void) => {
-            const item = {
-                input,
-                resolve
-            };
-            this.add(item);
-        });
-    }
+  }
+  call(input: I) {
+    return new Promise((resolve: (output: O) => void) => {
+      const item = {
+        input,
+        resolve
+      };
+      this.add(item);
+    });
+  }
 };
